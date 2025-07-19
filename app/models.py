90ff687eeed8e89269eec074b759d10a95bd3699
zxenonx <xenon.nemrod@gmail.com>
2025-07-19 19:10:44 +0100

    Add Book model

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    author = Column(String, index=True)
+    published_year = Column(Integer)
+    summary = Column(String, nullable=True)
